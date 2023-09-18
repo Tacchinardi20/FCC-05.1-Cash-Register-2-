5377890b29db305fe97111f52bb6cb41fe53c331
Prin Techarong <techarong.prin@gmail.com>
2023-09-18 16:08:00 +0700

    Updated

--- 05.1 Cash Register (2).js
+++ 05.1 Cash Register (2).js
@@ -1,24 +1,30 @@
 function checkCashRegister(price, cash, cid) {
-  // Set up variables
+
+  // Setup Variables
   var change = cash-price;
   var changeList = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
   var totalInRegister = 0;
   var denomination;
-  //calculating total in register
-  for(let i in cid){
+
+  //Calculating Total in Register
+  for(let i in cid) {
     totalInRegister+=cid[i][1];
   }
+
   //Check if register has enough money
-  if(totalInRegister<change){
+  if(totalInRegister<change) {
     return {status: "INSUFFICIENT_FUNDS", change: []}
   }
-  //check if register has the exact amount of money
-  else if(totalInRegister==change){
+
+  //Check if register has the exact amount of money
+  else if(totalInRegister==change) {
     return {status: "CLOSED", change: cid}
   }
-  //Selecting next denomination
-  for(let i = 8; i>=0;i--){
-    switch(i){
+
+  //Selecting Next Denomination
+  for(let i = 8; i>=0;i--) {
+
+    switch(i) {
       case 8: denomination = 100; break;
       case 7: denomination = 20; break;
       case 6: denomination = 10; break;
@@ -29,21 +35,26 @@ function checkCashRegister(price, cash, cid) {
       case 1: denomination = 0.05; break;
       case 0: denomination = 0.01; break;
     }
-    //giving the denomination if appropriate
-    while(cid[i][1]>=denomination && change>=denomination){
+
+    //Giving the denomination if appropriate
+    while(cid[i][1]>=denomination && change>=denomination) {
       cid[i][1] = Math.round((cid[i][1]*100)-(denomination*100))/100;
       changeList[i][1] = Math.round((changeList[i][1]*100)+(denomination*100))/100;
       change = Math.round((change*100)-(denomination*100))/100;
     }
-    if(changeList[i][1]==0){
+    if(changeList[i][1]==0) {
       changeList.splice(i,1);
     }
+
   }
+
   //If not all change was given
-  if(change>0){
+  if(change>0) {
     return {status: "INSUFFICIENT_FUNDS", change: []}
   }
+
   return {status: "OPEN", change: changeList.reverse()};
 }
+
 var answer = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 console.log(answer["status"] + "   ///   " + answer["change"]);
